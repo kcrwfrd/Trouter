d1bc6a8c4a400c3122da1b95562e5e40f6699d57
Kevin Crawford <kvcrawford@gmail.com>
2016-05-02 11:56:53 -0700

    feat(Route): add enter method to handle resolves and controller invocation

--- src/Route.js
+++ src/Route.js
@@ -1,18 +1,23 @@
+import _ from 'lodash'
+import {isObject, defer} from './common'
+
 /**
  * @class Route
  * @description
  * A fully-built route.
-**/
+ */
 
 class Route {
   constructor(definition) {
-    let {name, url, controller, parent, path} = definition
+    let {name, url, controller, parent, path, resolve} = definition
 
     this.name = name
     this.url = url
-    this.controller = controller
+    this.Controller = controller // Controller definition
+    this.controller = null // Controller instance
     this.parent = parent || null
     this.path = path.concat(this)
+    this.resolve = resolve || {}
   }
 
   getFqn() {
@@ -34,6 +39,81 @@ class Route {
   getRoot() {
     return this.parent && this.parent.getRoot() || this
   }
+
+  enter() {
+    let promise = resolve(this.resolve)
+
+    promise.then((value) => {
+      if (this.Controller) {
+        this.controller = new this.Controller(value)
+      }
+    })
+
+    return promise
+  }
+
+  exit() {
+    let promise = Promise.resolve(this.controller.onExit())
+
+    promise.then(() => this.controller = null)
+
+    return promise
+  }
+}
+
+/**
+ * @method resolve
+ * @private
+ * @description
+ * Invokes/normalizes resolve functions and values into promises.
+ *
+ * @param {*} value
+ *
+ * @returns {Promise}
+ */
+
+function resolve(value) {
+  if (value instanceof Promise) {
+    return value
+  }
+
+  if (typeof value === 'function') {
+    return Promise.resolve(value())
+  }
+
+  if (_.isPlainObject(value)) {
+    return resolveObject(value)
+  }
+
+  if (Array.isArray(value)) {
+    return Promise.all(value.map((item) => {
+      return (typeof item === 'function') ? item() : item
+    }))
+  }
+
+  return Promise.resolve(value)
+}
+
+function resolveObject(obj) {
+  let keys = Object.keys(obj)
+
+  // First invoke resolves and combine into single promise
+  let combinedPromise = Promise.all(keys.map((key) => {
+    let value = obj[key]
+
+    return (typeof value === 'function') ? value() : value
+  }))
+
+  // Then assign resolved values to their keys
+  return combinedPromise.then((values) => {
+    return values.reduce((memo, value, index) => {
+      let key = keys[index]
+
+      memo[key] = value
+
+      return memo
+    }, {})
+  })
 }
 
 export default Route
