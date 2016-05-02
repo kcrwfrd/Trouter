3e87f52b4621077f7f97455876c701d622a2180d
Kevin Crawford <kvcrawford@gmail.com>
2016-05-02 11:54:52 -0700

    test(Route): resolve promise handling

--- src/spec/Route.spec.js
+++ src/spec/Route.spec.js
@@ -1,4 +1,5 @@
 import Route from '../Route'
+import {defer} from '../common'
 
 describe('Route:', () => {
   it('Should properly instantiate.', () => {
@@ -12,5 +13,67 @@ describe('Route:', () => {
     expect(route).toBeDefined()
     expect(route.getRoot()).toBe(route)
     expect(route.getFqn()).toBe('foo')
+  })
+
+  describe('Resolve:', () => {
+    let controller, deferred, route;
+
+    beforeEach(() => {
+      controller = jasmine.createSpy('controller')
+      deferred = defer()
+
+      route = new Route({
+        name: 'foo',
+        controller: controller,
+        resolve: (() => deferred.promise),
+        path: []
+      })
+
+      route.enter()
+    })
+
+
+    it('Should resolve promises before controller instantiation.', () => {
+      expect(controller).not.toHaveBeenCalled()
+
+      deferred.resolve('bar')
+
+      return deferred.promise.then(() => {
+        expect(controller).toHaveBeenCalled()
+      })
+    })
+
+    it('Should instantiate the controller with resolved values as arguments.', () => {
+      deferred.resolve('bar')
+
+      return deferred.promise.then(() => {
+        expect(controller).toHaveBeenCalledWith('bar')
+      })
+    })
+
+    it('Should instantiate the controller with named resolves.', () => {
+      let foo = defer()
+      let bar = defer()
+
+      let route = new Route({
+        name: 'foo',
+        controller: controller,
+        resolve: {
+          foo: (() => foo.promise),
+          bar: (() => bar.promise)
+        },
+        path: [],
+      })
+
+      foo.resolve('Foo')
+      bar.resolve('Bar')
+
+      return route.enter().then(() => {
+        expect(controller.calls.mostRecent().args[0]).toEqual({
+          foo: 'Foo',
+          bar: 'Bar',
+        })
+      })
+    })
   })
 })
