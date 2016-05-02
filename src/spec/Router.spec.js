04b923e67d9000c1cb4ebb10eea8bce5a85c8236
Kevin Crawford <kvcrawford@gmail.com>
2016-05-02 12:09:47 -0700

    test(Router): route tree traversal and resolve promise handling when navigating to route

--- src/spec/Router.spec.js
+++ src/spec/Router.spec.js
@@ -1,4 +1,5 @@
 import Router from '../Router'
+import {defer} from '../common'
 
 describe('Router:', () => {
   let router, homeCtrl, childCtrl, siblingCtrl, grandChildCtrl, fooCtrl;
@@ -10,6 +11,7 @@ describe('Router:', () => {
     childCtrl = jasmine.createSpy()
     siblingCtrl = jasmine.createSpy()
     grandChildCtrl = jasmine.createSpy()
+    fooCtrl = jasmine.createSpy()
 
     router.route('home', { controller: homeCtrl })
       .route('home.child', { controller: childCtrl })
@@ -20,37 +22,57 @@ describe('Router:', () => {
 
   describe('go(name):', () => {
     it('Should invoke the controller bound to the route.', () => {
-      router.go('home')
-
-      expect(homeCtrl).toHaveBeenCalled()
+      return router.go('home').then(() => {
+        expect(homeCtrl).toHaveBeenCalled()
+      })
     })
 
     it('Should invoke a parent controller when a child is navigated to', () => {
-      router.go('home.child')
-
-      expect(homeCtrl).toHaveBeenCalled()
-      expect(childCtrl).toHaveBeenCalled()
+      return router.go('home.child').then(() => {
+        expect(homeCtrl).toHaveBeenCalled()
+        expect(childCtrl).toHaveBeenCalled()
+      })
     })
 
     it('Should not invoke parent controller a second time when navigating to child.', () => {
-      router.go('home')
-
-      expect(homeCtrl).toHaveBeenCalled()
-
-      router.go('home.child')
-
-      expect(childCtrl).toHaveBeenCalled()
-      expect(homeCtrl.calls.count()).toBe(1)
+      return router.go('home')
+        .then(() => router.go('home.child'))
+        .then(() => {
+          expect(childCtrl).toHaveBeenCalled()
+          expect(homeCtrl.calls.count()).toBe(1)
+        })
     })
 
     it('Should not invoke parent controller a second time when navigating to sibling.', () => {
-      router.go('home.child')
+      return router.go('home.child')
+        .then(() => router.go('home.sibling'))
+        .then(() => {
+          expect(homeCtrl.calls.count()).toBe(1)
+          expect(childCtrl.calls.count()).toBe(1)
+          expect(siblingCtrl.calls.count()).toBe(1)
+        })
+    })
 
-      router.go('home.sibling')
+    it('Should not invoke parent controller a second time when go is called synchronously.', () => {
+      router.go('home')
 
-      expect(homeCtrl.calls.count()).toBe(1)
-      expect(childCtrl.calls.count()).toBe(1)
-      expect(siblingCtrl.calls.count()).toBe(1)
+      return router.go('home.child')
+        .then(() => {
+          expect(homeCtrl.calls.count()).toBe(1)
+          expect(childCtrl.calls.count()).toBe(1)
+        })
+    })
+
+    it('Should invoke parent controller a second time', () => {
+      return router.go('home.child')
+        .then(() => router.go('foo'))
+        .then(() => router.go('home.child.grandChild'))
+        .then(() => {
+          expect(fooCtrl.calls.count()).toBe(1)
+          expect(homeCtrl.calls.count()).toBe(2)
+          expect(childCtrl.calls.count()).toBe(2)
+          expect(grandChildCtrl.calls.count()).toBe(1)
+        })
     })
 
     it('Should exit and enter the correct paths.', () => {
@@ -62,6 +84,67 @@ describe('Router:', () => {
 
     it('Should throw an error when route not found.', () => {
       expect(() => router.go('wat')).toThrow(new Error("Route 'wat' not found."))
+    })
+
+    describe('With resolve:', () => {
+      let controller, deferred, router;
+
+      beforeEach(() => {
+        controller = jasmine.createSpy('controller')
+        deferred = defer()
+        router = new Router()
+
+        router.route('foo', {
+          controller: controller,
+          resolve: () => {
+            return deferred.promise
+          }
+        })
+      })
+
+      it('Should invoke the controller after promise resolution.', () => {
+        router.go('foo')
+
+        expect(controller).not.toHaveBeenCalled()
+
+        deferred.resolve('bar')
+
+        return deferred.promise.then(() => {
+          expect(controller).toHaveBeenCalled()
+        })
+      })
+
+      it('Should not invoke the controller after promise rejection.', () => {
+        // @TODO: emit error event
+        // let onError = jasmine.createSpy('onError')
+        // router.on('error', onError)
+
+        router.go('foo')
+
+        deferred.reject('bar')
+
+        return deferred.promise.catch(() => {
+          expect(controller).not.toHaveBeenCalled()
+          // expect(onError).toHaveBeenCalled()
+        })
+      })
+
+      it('Should not invoke a child route if parent resolve was rejected.', () => {
+        let controller = jasmine.createSpy('controller')
+
+        router.route('foo.bar', {
+          parent: 'foo',
+          controller: controller
+        })
+
+        router.go('foo.bar')
+
+        deferred.reject('rejected')
+
+        return deferred.promise.catch(() => {
+          expect(controller).not.toHaveBeenCalled()
+        })
+      })
     })
   })
 })
