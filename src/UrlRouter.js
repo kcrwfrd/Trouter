ffff821e32e98c574453a739988be75d0a09499c
Kevin Crawford <kvcrawford@gmail.com>
2016-05-04 16:34:40 -0700

    feat(UrlRouter): bind route patterns to handlers

---
+++ src/UrlRouter.js
@@ -0,0 +1,45 @@
+import UrlMatcher from './UrlMatcher'
+
+class UrlRouter {
+  constructor(prefix) {
+    this.prefix = prefix || '#!'
+    this.rules = []
+  }
+
+  onChange(hash) {
+    let path = hash.replace(this.prefix, '')
+
+    for (let rule of this.rules) {
+      if (rule(path)) {
+        return true
+      }
+    }
+
+    throw new Error(`No route handler found for '${path}'`)
+  }
+
+  listen() {
+    // @TODO: normalize popstate/hashchange
+    window.addEventListener('hashchange', (event) => {
+      this.onChange(window.location.hash)
+    })
+  }
+
+  when(url, handler) {
+    let matcher = new UrlMatcher(url)
+
+    this.rules.push((path) => {
+      let result = matcher.exec(path)
+
+      if (!result) {
+        return false
+      }
+
+      handler(result)
+
+      return true
+    })
+  }
+}
+
+export default UrlRouter
